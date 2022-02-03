e5928ae3c9cea4ba5864999f121a2449592e4629
RakhmanIbrayev <rakhmanxibrayev@gmail.com>
2022-02-04 01:47:18 +0600

    fix the non-stop drawing bug

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
+let setting = "draw";
+let currentGridSide = 16;
+let mouseDown = false;
 const grid = document.querySelector(".grid");
 const sizeOutput = document.querySelector(".size-text");
 const sizeSlider = document.querySelector(".size-slider");
 const btnDraw = document.querySelector(".btn-draw");
 const btnEraser = document.querySelector(".btn-eraser");
 const btnClear = document.querySelector(".btn-clear");
-let setting = "draw";
-let currentGridSide = 16;
 
 function removeGrid () {
     grid.innerHTML = "";
 }
 
-let mouseDown = false;
-document.body.onmousedown = () => (mouseDown = true);
-document.body.onmouseup = () => (mouseDown = false);
+function applySetting (e) {
+    if (!mouseDown) {
+        return;
+    }
+
+    if (setting === "draw") {
+        e.target.style.backgroundColor = "#1f2937";
+    } else if (setting === "eraser") {
+        e.target.style.backgroundColor = "white";
+    }
+}
 
 function createNewGrid (gridSide) {
     const gridSize = gridSide * gridSide;
@@ -23,21 +32,8 @@ function createNewGrid (gridSide) {
         const box = document.createElement("div");
         box.style.width = boxSize + "%";
         box.style.height = boxSize + "%";
-        box.addEventListener("mouseover", applySetting);
-        box.addEventListener("mousedown", applySetting);
+        box.onmouseenter = applySetting;
         grid.append(box);
-    }
-}
-
-function applySetting (e) {
-    if (e.type === "mouseover" && !mouseDown) {
-        return;
-    }
-
-    if (setting === "draw") {
-        e.target.style.backgroundColor = "black";
-    } else if (setting === "eraser") {
-        e.target.style.backgroundColor = "white";
     }
 }
 
@@ -49,6 +45,13 @@ function updateGrid (gridSide) {
 function changeSizeOutput (gridSide) {
     sizeOutput.textContent = gridSide + " x " + gridSide;
 }
+
+grid.onmousedown = (e) => {
+    mouseDown = true;
+    e.preventDefault();
+};
+
+grid.onmouseup = () => mouseDown = false;
 
 btnDraw.addEventListener("click", () => setting = "draw");
 btnClear.addEventListener("click", () => updateGrid(currentGridSide));
